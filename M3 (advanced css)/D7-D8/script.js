c2471a552cd0558491551399d098237f05805372
GabrieleCannavale <gabriele.cannavale92@gmail.com>
2023-05-15 02:57:36 +0200

    m3 completato

--- M3 (advanced css)/D7-D8/script.js
+++ M3 (advanced css)/D7-D8/script.js
@@ -1,18 +1,12 @@
+//? focus list card
+let cards = document.querySelectorAll('.card');
 
+for (let card of cards) {
+  card.addEventListener("mouseover", (event) => {
+    card.classList.add("focus-over");
+  })
 
-let items = document.querySelectorAll('.carousel .carousel-item')
-
-		items.forEach((el) => {
-			const minPerSlide = 6
-			let next = el.nextElementSibling
-			for (var i=1; i<minPerSlide; i++) {
-				if (!next) {
-            // wrap carousel by using first child
-            next = items[0]
-        }
-        let cloneChild = next.cloneNode(true)
-        el.appendChild(cloneChild.children[0])
-        next = next.nextElementSibling
-    }
-})
-
+  card.addEventListener("mouseout", (event) => {
+    card.classList.remove("focus-over");
+  });
+}
